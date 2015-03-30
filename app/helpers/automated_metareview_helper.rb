0d3e64a0b2cc71f1b2c5873c1805378583394749
AncientMariner <AM@localhost.localdomain>
2015-03-29 22:39:26 -0400

    Migrated the original project to web project.
    Web service branch is migrated to web project.

---
+++ app/helpers/automated_metareview_helper.rb
@@ -0,0 +1,2 @@
+module AutomatedMetareviewHelper
+end
---
+++ test/controllers/automated_metareview_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class AutomatedMetareviewControllerTest < ActionController::TestCase
+  test "should get AutomatedMetareview" do
+    get :AutomatedMetareview
+    assert_response :success
+  end
+
+end
