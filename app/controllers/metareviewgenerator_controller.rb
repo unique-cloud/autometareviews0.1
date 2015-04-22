cb228f42e8bd5cf4c8859af88df153dbe1d7e7a0
rkyadav-ncsu <rkyadav@ncsu.edu>
2015-04-22 19:58:05 -0400

    removed unnecessary code

--- app/controllers/metareviewgenerator_controller.rb
+++ app/controllers/metareviewgenerator_controller.rb
@@ -20,35 +20,6 @@ require 'review_coverage'
 class MetareviewgeneratorController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   respond_to :json
-  def AutomatedMetareview
-    #puts param[:qty]
-    #class driver
-    #run the code from here
-    preprocess = TextPreprocessing.new
-    review_array = preprocess.fetch_data("app/data/reviews.csv")
-    submission_array = preprocess.fetch_data("app/data/submission.csv")
-    rubricqns_array = preprocess.fetch_data("app/data/rubric.csv")
-    #setting up the output file
-    output_file = "app/data/output-sample.csv"
-    csvWriter = CSV.open(output_file, "w")
-    csvWriter << ["Review", "Submission", "Plagiarism", "Relevance", "Summative Content", "Problem Content",
-                  "Advisory Content", "Coverage", "Positive Tone", "Negative Tone", "Neutral Tone", "Quantity"]
-
-    for i in (0..review_array.length - 1)
-      autometareview = Automated_Metareview.new
-      review = Array.new
-      submission = Array.new
-      review << review_array[i]
-      submission << submission_array[i]
-      features = autometareview.calculate_metareview_metrics(review, submission, rubricqns_array)
-      #write the features out to a file
-      csvWriter << [review_array[i], submission_array[i], features["plagiarism"], features["relevance"], features["content_summative"],
-                    features["content_problem"], features["content_advisory"], features["coverage"], features["tone_positive"],
-                    features["tone_negative"], features["tone_neutral"], features["quantity"]]
-      render json: features.to_json
-    end
-  end
-  respond_to :json
   def create
     puts params[:qty]
 
