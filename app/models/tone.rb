fdf6f8e64719bfcd40d4d849e65b43917f37f4ea
sramase <sramase@ncsu.edu>
2015-04-23 19:24:22 -0400

    Wordnet Refactor - Ruby style loops and names

--- app/models/tone.rb
+++ app/models/tone.rb
@@ -150,7 +150,7 @@ class Tone
   def get_synonyms(vertex, threshold, speller)
     wbsim = WordnetBasedSimilarity.new
     if(vertex.pos_tag.nil?)
-      pos = wbsim.determine_POS(vertex)
+      pos = wbsim.determine_pos(vertex)
     else
       pos = vertex.pos_tag
     end
