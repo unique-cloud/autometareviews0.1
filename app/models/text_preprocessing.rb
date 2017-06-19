b56cbbf57add53db11cee2732a2a8286c3969b0a
MILKSHAK <maxinghua123000@gmail.com>
2017-06-19 11:43:11 -0400

    volume code clean up for readability

--- app/models/text_preprocessing.rb
+++ app/models/text_preprocessing.rb
@@ -5,26 +5,23 @@ require 'csv'
 
 class TextPreprocessing
 
-=begin
- Fetching review data from the tables based on the response_map id 
-=end
+  # Fetching review data from the tables based on the response_map id
   def fetch_data(filename)
     data_array = Array.new
     CSV.foreach(filename) do |row|
       data_array << row[0]
-#      puts row[0]
     end
     return data_array
   end
-#------------------------------------------#------------------------------------------#------------------------------------------  
-=begin
-  pre-processes the review text and sends it in for graph formation and further analysis
-=end
+
+  # pre-processes the review text and sends it in for graph formation
+  # and further analysis
   def segment_text(train_or_test_reviews, text_array)
-    if(train_or_test_reviews == 0)
+    if (train_or_test_reviews == 0)
       reviews = Array.new(1){Array.new}
     else
-      reviews = Array.new(50){Array.new} #50 is the number of different reviews/submissions
+      # 50 is the number of different reviews/submissions
+      reviews = Array.new(50){Array.new}
     end
 
     i = 0
@@ -32,94 +29,96 @@ class TextPreprocessing
 
     for k in (0..text_array.length-1)
       text = text_array[k]
-      if(train_or_test_reviews == 1) #reset i (the sentence counter) to 0 for test reviews
-        reviews[j] = Array.new #initializing the array for sentences in a test review
+      # reset i (the sentence counter) to 0 for test reviews
+      if (train_or_test_reviews == 1)
+        # initializing the array for sentences in a test review
+        reviews[j] = Array.new
         i = 0
       end
 
-      #******* Pre-processing the review/submission text **********
-      #replacing commas in large numbers, makes parsing sentences with commas confusing!
-      #replacing quotation marks
-      #replacing gsub with delete(as stated by RUBOCOP)
+      # ******* Pre-processing the review/submission text **********
+      # replacing commas in large numbers, makes parsing sentences with commas confusing!
+      # replacing quotation marks
+      # replacing gsub with delete(as stated by RUBOCOP)
       text.delete!("\"")
       text.delete!("(")
       text.delete!(")")
-      if(text.include?("http://"))
+      if (text.include?("http://"))
         text = remove_urls(text)
       end
-      #break the text into multiple sentences
+      # break the text into multiple sentences
       beginn = 0
       # replaced multiple logical ors with any
-      if ([".","?","!",",",";"].any? { |ch| (text).include?(ch)}) #new clause or sentence
-        while([".","?","!",",",";"].any? { |ch| (text).include?(ch)}) do #the text contains more than 1 sentence
+      # new clause or sentence
+      if ([".","?","!",",",";"].any? { |ch| (text).include?(ch)})
+        # the text contains more than 1 sentence
+        while ([".","?","!",",",";"].any? { |ch| (text).include?(ch)}) do
           endd = 0
-          #these 'if' conditions have to be independent, cause the value of 'endd' could change for the different types of punctuations
-          if(text.include?("."))
+          # these 'if' conditions have to be independent, cause the value of
+          # 'endd' could change for the different types of punctuations
+          if (text.include?("."))
             endd = text.index(".")
           end
-          #removed duplicate code by exytracting a method find_occurance
-          endd= find_occurance(text,"?") #if a ? occurs before a .
-          endd= find_occurance(text,"!")#if an ! occurs before a . or a ? 
-          endd= find_occurance(text,",")#if an , occurs before a . or a ? or !
-          endd= find_occurance(text,";")#if an , occurs before a . or a ? or ! or ,
+          # removed duplicate code by exytracting a method find_occurance
+          endd = find_occurance(text,"?") # if a ? occurs before a .
+          endd = find_occurance(text,"!") # if an ! occurs before a . or a ?
+          endd = find_occurance(text,",") # if an , occurs before a . or a ? or !
+          endd = find_occurance(text,";") # if an , occurs before a . or a ? or ! or ,
          
-          #check if the string between two commas or punctuations is there to buy time e.g. ", say," ",however," ", for instance, "...
-          if(train_or_test_reviews == 0) #training
+          # check if the string between two commas or punctuations is there to buy time e.g. ", say," ",however," ", for instance, "...
+          if (train_or_test_reviews == 0) # training
             reviews[0][i] = text[beginn..endd].strip
-          else #testing
+          else # testing
             reviews[j][i] = text[beginn..endd].strip
           end
-          i+=1 #incrementing the sentence counter
-          text = text[(endd+1)..text.length] #from end+1 to the end of the string variable
+          i += 1 # incrementing the sentence counter
+          text = text[(endd+1)..text.length] # from end+1 to the end of the string variable
         end
       else
-        if(train_or_test_reviews == 0)#training
+        if (train_or_test_reviews == 0) # training
           reviews[0][i] = text.strip
-          i+=1
+          i += 1
         else
           reviews[j][i] = text.strip
         end
       end
 
-      if(train_or_test_reviews == 1)#incrementing reviews counter only for test reviews
-        j+=1
+      if (train_or_test_reviews == 1) # incrementing reviews counter only for test reviews
+        j += 1
       end
     end
 
-    #setting the number of reviews before returning
-    if(flag == 0)
+    # setting the number of reviews before returning
+    if (flag == 0)
       num_reviews = 1
     else
       num_reviews = j
     end
 
-    if(flag == 0)
+    if (flag == 0)
       return reviews[0]
     end
   end
- #Extract method: function to replace multiple ifs. to remove code duplication
+ # Extract method: function to replace multiple ifs. to remove code duplication
  def find_occurance(text,char)
   if((text.include?(char) and endd != 0 and endd > text.index(char)) or (text.include?(char) and endd == 0))
             endd = text.index(char)
   end
- return endd
+  return endd
  end
-#------------------------------------------#------------------------------------------#------------------------------------------
-=begin
-   * Reads the patterns from the csv file containing them. 
-   * maxValue is the maximum value of the patterns found
-=end
 
+  # Reads the patterns from the csv file containing them.
+  # maxValue is the maximum value of the patterns found
   def read_patterns(filename, pos)
-    num = 1000 #some large number
+    num = 1000 # some large number
     patterns = Array.new
     state = POSITIVE
-    i = 0 #keeps track of the number of edges
+    i = 0 # keeps track of the number of edges
 
-    #setting the state for problem detection and suggestive patterns
-    if(filename.include?("prob"))
+    # setting the state for problem detection and suggestive patterns
+    if (filename.include?("prob"))
       state = NEGATED
-    elsif(filename.include?("suggest"))
+    elsif (filename.include?("suggest"))
       state = SUGGESTIVE
     end
 
@@ -127,24 +126,26 @@ class TextPreprocessing
       in_vertex = text[0][0..text[0].index("=")-1].strip
       out_vertex = text[0][text[0].index("=")+2..text[0].length].strip
 
-      first_string_in_vertex = pos.get_readable(in_vertex.split(" ")[0]) #getting the first token in vertex to determine POS
-      first_string_out_vertex = pos.get_readable(out_vertex.split(" ")[0]) #getting the first token in vertex to determine POS
+      # getting the first token in vertex to determine POS
+      first_string_in_vertex = pos.get_readable(in_vertex.split(" ")[0])
+      # getting the first token in vertex to determine POS
+      first_string_out_vertex = pos.get_readable(out_vertex.split(" ")[0])
 
       patterns[i] = Edge.new("noun", NOUN)
-      #setting the invertex
-      if(first_string_in_vertex.include?("/NN") or first_string_in_vertex.include?("/PRP") or first_string_in_vertex.include?("/IN") or first_string_in_vertex.include?("/EX") or first_string_in_vertex.include?("/WP"))
+      # setting the invertex
+      if (first_string_in_vertex.include?("/NN") or first_string_in_vertex.include?("/PRP") or first_string_in_vertex.include?("/IN") or first_string_in_vertex.include?("/EX") or first_string_in_vertex.include?("/WP"))
         patterns[i].in_vertex = Vertex.new(in_vertex, NOUN, i, state, nil, nil, first_string_in_vertex[first_string_in_vertex.index("/")+1..first_string_in_vertex.length])
-      elsif(first_string_in_vertex.include?("/VB") or first_string_in_vertex.include?("MD"))
+      elsif (first_string_in_vertex.include?("/VB") or first_string_in_vertex.include?("MD"))
         patterns[i].in_vertex = Vertex.new(in_vertex, VERB, i, state, nil, nil, first_string_in_vertex[first_string_in_vertex.index("/")+1..first_string_in_vertex.length])
-      elsif(first_string_in_vertex.include?("JJ"))
+      elsif (first_string_in_vertex.include?("JJ"))
         patterns[i].in_vertex = Vertex.new(in_vertex, ADJ, i, state, nil, nil, first_string_in_vertex[first_string_in_vertex.index("/")+1..first_string_in_vertex.length])
-      elsif(first_string_in_vertex.include?("/RB"))
+      elsif (first_string_in_vertex.include?("/RB"))
         patterns[i].in_vertex = Vertex.new(in_vertex, ADV, i, state, nil, nil, first_string_in_vertex[first_string_in_vertex.index("/")+1..first_string_in_vertex.length])
-      else #default to noun
+      else # default to noun
         patterns[i].in_vertex = Vertex.new(in_vertex, NOUN, i, state, nil, nil, first_string_in_vertex[first_string_in_vertex.index("/")+1..first_string_in_vertex.length])
       end
 
-      #setting outvertex
+      # setting outvertex
       if(first_string_out_vertex.include?("/NN") or first_string_out_vertex.include?("/PRP") or first_string_out_vertex.include?("/IN") or first_string_out_vertex.include?("/EX") or first_string_out_vertex.include?("/WP"))
         patterns[i].out_vertex = Vertex.new(out_vertex, NOUN, i, state, nil, nil, first_string_out_vertex[first_string_out_vertex.index("/")+1..first_string_out_vertex.length])
       elsif(first_string_out_vertex.include?("/VB") or first_string_out_vertex.include?("MD"))
@@ -153,27 +154,23 @@ class TextPreprocessing
         patterns[i].out_vertex = Vertex.new(out_vertex, ADJ, i, state, nil, nil, first_string_out_vertex[first_string_out_vertex.index("/")+1..first_string_out_vertex.length-1]);
       elsif(first_string_out_vertex.include?("/RB"))
         patterns[i].out_vertex = Vertex.new(out_vertex, ADV, i, state, nil, nil, first_string_out_vertex[first_string_out_vertex.index("/")+1..first_string_out_vertex.length])
-      else #default is noun
+      else # default is noun
         patterns[i].out_vertex = Vertex.new(out_vertex, NOUN, i, state, nil, nil, first_string_out_vertex[first_string_out_vertex.index("/")+1..first_string_out_vertex.length])
       end
-      i+=1
+      i += 1
     end
     num_patterns = i
     return patterns
   end
 
-#------------------------------------------#------------------------------------------#------------------------------------------
-
-=begin
- Removes any urls in the text and returns the remaining text as it is 
-=end
+  # Removes any urls in the text and returns the remaining text as it is
   def remove_urls(text)
     final_text = String.new
-    if(text.include?("http://"))
+    if (text.include?("http://"))
       tokens = text.split(" ")
       tokens.each{
           |token|
-        if(!token.include?("http://"))
+        if (!token.include?("http://"))
           final_text = final_text + " " + token
         end
       }
@@ -182,44 +179,40 @@ class TextPreprocessing
     end
     return final_text
   end
-#------------------------------------------#------------------------------------------#------------------------------------------
 
-=begin
-Check for plagiarism after removing text within quotes for reviews
-=end
+  # Check for plagiarism after removing text within quotes for reviews
   def remove_text_within_quotes(review_text)
     # puts "Inside removeTextWithinQuotes:: "
     reviews = Array.new
     review_text.each{ |row|
       text = row
-      #the read text is tagged with two sets of quotes!
-      if(text.include?("\""))
-        while(text.include?("\"")) do
+      # the read text is tagged with two sets of quotes!
+      if (text.include?("\""))
+        while (text.include?("\"")) do
           replace_text = text.scan(/"([^"]*)"/)
-          #fetching the start index of the quoted text, in order to replace the complete segment
-          start_index = text.index(replace_text[0].to_s) - 1 #-1 in order to start from the quote
+          # fetching the start index of the quoted text, in order to replace the complete segment
+          # -1 in order to start from the quote
+          start_index = text.index(replace_text[0].to_s) - 1
           text.gsub!(text[start_index..start_index + replace_text[0].to_s.length+1], "")
         end
       end
-      reviews << text #set the text after all quoted segments have been removed.
+      reviews << text # set the text after all quoted segments have been removed.
     }
-    return reviews #return only the first array element - a string!
+    return reviews # return only the first array element - a string!
   end
-#------------------------------------------#------------------------------------------#------------------------------------------   
-=begin
- Looks for spelling mistakes in the text and fixes them using the raspell library available for ruby 
-=end
+
+  # Looks for spelling mistakes in the text and fixes them using the raspell library available for ruby
   def check_correct_spellings(review_text_array, speller)
     review_text_array_temp = Array.new
-    #iterating through each response
+    # iterating through each response
     review_text_array.each{
         |review_text|
       review_tokens = review_text.split(" ")
       review_text_temp = ""
-      #iterating through tokens from each response
+      # iterating through tokens from each response
       review_tokens.each{
           |review_tok|
-        #checkiing the stem word's spelling for correctness
+        # checkiing the stem word's spelling for correctness
         if(!speller.correct?(review_tok))
           if(!speller.suggestions(review_tok).first.nil?)
             review_tok = speller.suggestions(review_tok).first
@@ -232,30 +225,26 @@ Check for plagiarism after removing text within quotes for reviews
     return review_text_array_temp
   end
 
-#------------------------------------------#------------------------------------------#------------------------------------------
-=begin
- Checking if "str" is a punctuation mark like ".", ",", "?" etc. 
-=end
-  public #The method was throwing a "NoMethodError: private method" error when called from a different class. Hence the "public" keyword.
+  # Checking if "str" is a punctuation mark like ".", ",", "?" etc.
+  # The method was throwing a "NoMethodError: private method" error when called
+  # from a different class. Hence the "public" keyword.
+  public
   def contains_punct(str)
     str.delete!(".,?!;:()[]")
     return str
   end
 
   def contains_punct_bool(str)
-    if(str.include?("\\n") or str.include?("}") or str.include?("{"))
+    if (str.include?("\\n") or str.include?("}") or str.include?("{"))
       return true
     else
       return false
     end
   end
 
-#------------------------------------------#------------------------------------------#------------------------------------------
-=begin
- Checking if "str" is a punctuation mark like ".", ",", "?" etc. 
-=end
+  # Checking if "str" is a punctuation mark like ".", ",", "?" etc.
   def is_punct(str)
-    if(str == "." or str == "," or str == "?" or str == "!" or str == ";" or str == ":")
+    if (str == "." or str == "," or str == "?" or str == "!" or str == ";" or str == ":")
       return true
     else
       return false
--- app/models/text_quantity.rb
+++ app/models/text_quantity.rb
@@ -3,8 +3,11 @@ require 'text_preprocessing'
 
 class TextQuantity
   def number_of_unique_tokens(text_array)
-    pre_string = "" #preString helps keep track of the text that has been checked for unique tokens and text that has not
-    count = 0 #counts the number of unique tokens
+    # preString helps keep track of the text that has been checked for unique
+    # tokens and text that has not
+    pre_string = ""
+    # counts the number of unique tokens
+    count = 0
     instance = WordnetBasedSimilarity.new
     text_array.each{
       |text|
@@ -13,12 +16,15 @@ class TextQuantity
       all_tokens = text.split(" ")
       all_tokens.each{ 
         |token|
-        if(!instance.is_frequent_word(token.downcase)) #do not count this word if it is a frequent word
-          if(!pre_string.downcase.include?(token.downcase)) #if the token was not already seen earlier i.e. not a part of the preString
-            count+=1
+        # do not count this word if it is a frequent word
+        if (!instance.is_frequent_word(token.downcase))
+          # if the token was not already seen earlier i.e. not a part of the preString
+          if (!pre_string.downcase.include?(token.downcase))
+            count += 1
           end  
-        end  
-        pre_string = pre_string +" " + token.downcase #adding token to the preString
+        end
+        # adding token to the preString
+        pre_string = pre_string + " " + token.downcase
       }
     }
     return count
